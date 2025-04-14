b553cd4180fc31545eaf01f90e3bfc07261b505f
Absolute-2004 <mixalobach@gmail.com>
2025-04-14 23:26:07 +0300

    feat: pass task retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -140,8 +140,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        const result = func();
+        return result;
+      } catch (e) {
+        if (i === attempts - 1) throw e;
+      }
+    }
+    return undefined;
+  };
 }
 
 /**
